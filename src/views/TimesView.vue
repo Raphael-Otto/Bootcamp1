c081af277f7d6bab081f955dfb7197fa01c1d6a5
Raphis <101464708+Raphael-Otto@users.noreply.github.com>
2022-06-09 14:52:00 -0300

    Add files via upload

--- src/views/TimesView.vue
+++ src/views/TimesView.vue
@@ -1,3 +1,19 @@
+<script>
+export default {
+  data() {
+    return {
+      times: [
+        { id: "8ae6623d-5462-4774-b5e1-e263c5d2d367", nome: "Time 1" },
+        { id: "1f659d91-17b5-40da-b5fb-dbffa5664e27", nome: "Time 2" },
+        { id: "d5a59ada-da1d-47a2-9a6e-e99964df9571", nome: "Time 3" },
+        { id: "2311c938-42fa-4661-a3fc-9ba664a30f6e", nome: "Time 4" },
+        { id: "6f58a76b-0ea0-4bca-8d93-296c7d69685c", nome: "Time 5" },
+      ],
+    };
+  },
+};
+</script>
+
 <template>
   <div class="container">
     <div class="title">
@@ -17,19 +33,9 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Time 1</td>
-            <td>??</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Time 2</td>
-            <td>??</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Time 3</td>
+          <tr v-for="times in times" :key="times.id">
+            <td>{{ times.id }}</td>
+            <td>{{ times.nome }}</td>
             <td>??</td>
           </tr>
         </tbody>
@@ -92,7 +98,7 @@ table tr td {
   padding: 10px;
 }
 
-table tr:nth-child(even) {
+table tr:nth-child(odd) {
   background-color: #ccc;
 }
 </style>
